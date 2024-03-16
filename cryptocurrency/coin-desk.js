013e26e47c9f9f2866bc36ae024a2ccf2bcadb57
tukky-14 <bigoonage0104@gmail.com>
2024-03-16 11:01:43 +0900

    coin-deskを追加

---
+++ cryptocurrency/coin-desk.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+async function getRequest() {
+    let response;
+    try {
+        response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRequest();
