95b1bd08cf08c73502c067d7b21647ec25d166d3
tukky-14 <bigoonage0104@gmail.com>
2024-03-16 12:30:13 +0900

    Jakan APIの処理を更新

--- apis/anime/jikan.js
+++ apis/anime/jikan.js
@@ -1,5 +1,10 @@
 const axios = require('axios');
 
+/**
+ * アニメのタイトルを使用してMyAnimeListのAPIを検索する関数
+ * @param {*} title
+ * @returns
+ */
 async function searchAnimeByTitle(title) {
     try {
         const response = await axios.get(
@@ -7,9 +12,7 @@ async function searchAnimeByTitle(title) {
         );
         if (response.data.data.length > 0) {
             const anime = response.data.data[0];
-            console.log('Found Anime:', anime.title);
-            console.log('Anime ID:', anime.mal_id);
-            return anime.mal_id; // さらに詳細情報を取得するために使用できる
+            return anime.mal_id;
         } else {
             console.log('No anime found with that title.');
             return null;
@@ -19,24 +22,25 @@ async function searchAnimeByTitle(title) {
     }
 }
 
-searchAnimeByTitle('進撃の巨人');
+/**
+ * MyAnimeListのアニメIDを使用して情報を取得する関数
+ */
+async function getAnimeDetails(animeTitle) {
+    try {
+        const animeId = await searchAnimeByTitle(animeTitle);
 
-// // MyAnimeListのアニメIDを使用して情報を取得する関数
-// async function getAnimeDetails(animeId) {
-//     try {
-//         // Jikan APIのアニメ詳細エンドポイントにリクエストを送る
-//         const response = await axios.get(`https://api.jikan.moe/v4/anime/${animeId}`);
+        // Jikan APIのアニメ詳細エンドポイントにリクエストを送る
+        const response = await axios.get(`https://api.jikan.moe/v4/anime/${animeId}`);
 
-//         // 取得したアニメの情報をコンソールに表示
-//         console.log('Title:', response.data.data.title);
-//         console.log('Synopsis:', response.data.data.synopsis);
-//         console.log('Episodes:', response.data.data.episodes);
-//         console.log('Score:', response.data.data.score);
-//         // 他にも多くの情報が取得できます
-//     } catch (error) {
-//         console.error('An error occurred:', error);
-//     }
-// }
+        // 取得したアニメの情報をコンソールに表示
+        console.log('Title:', response.data.data.title);
+        console.log('Synopsis:', response.data.data.synopsis);
+        console.log('Episodes:', response.data.data.episodes);
+        console.log('Score:', response.data.data.score);
+        // 他にも多くの情報が取得できます
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
 
-// // アニメIDを指定して関数を実行（例：1は「カウボーイビバップ」のID）
-// getAnimeDetails(16498);
+getAnimeDetails('進撃の巨人');
