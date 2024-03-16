4c8476dabdc7085c2a255c7c6216a78ccd6b0bd6
tukky-14 <bigoonage0104@gmail.com>
2024-03-16 11:01:55 +0900

    prettierファイルを修正

---
+++ music/lyrics-ovh.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+async function getRequest() {
+    let response;
+    try {
+        response = await axios.get(
+            'https://api.lyrics.ovh/v1/Coldplay/Adventure%20of%20a%20Lifetime'
+        );
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRequest();
